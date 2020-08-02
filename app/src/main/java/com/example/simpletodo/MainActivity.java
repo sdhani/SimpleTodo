4b5ec1ba1b0ede4ee6d91fd04b8692aa5b4f160f
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-08-02 13:01:50 -0400

    basic app intialization

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button addBtn;
+    EditText etItem;
+    RecyclerView rvItem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Have fun!");
+    }
+}
